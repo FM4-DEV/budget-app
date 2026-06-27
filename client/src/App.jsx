8742342f0da80ff51796916601a1d840fa5bb288
FM4-DEV <macafm4@gmail.com>
2026-06-27 17:35:05 -0400

    feat: added tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [message, setMessage] = useState("Loading...")
+  const [message, setMessage] = useState("Loading...");
 
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/hello_world")
-        .then(response => {
-            setMessage(response.data.message)
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-            setMessage("Failed to load data from backend.")
-        });
-    }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/hello_world")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("Failed to load data from backend.");
+      });
+  }, []);
 
-    return (
-        <div>{message}</div>
-    );
+  return <div className="bg-black text-white">{message}</div>;
 }
 
 export default App;
